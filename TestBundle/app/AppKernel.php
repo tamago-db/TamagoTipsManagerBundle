4c8fa160ea474050865841e8d7c01dc39611a076
Shwetha999 <shwethanagaraja@Shwethas-MacBook-Air.local>
2015-11-19 20:36:45 -0500

    Committing a dummy tip

--- TestBundle/app/AppKernel.php
+++ TestBundle/app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Lexik\Bundle\TranslationBundle\LexikTranslationBundle(),
+            new TipBundle\TestBundle\TipBundleTestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
