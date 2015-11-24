50a23c97fe778056f54f5e92a1e049c109949bfa
Sanskriti Jain <sanskritijain29@gmail.com>
2015-11-23 19:04:55 -0500

    Rearranging directory structure

--- TestBundle/README.md
+++
@@ -1,4 +0,0 @@
-TestBundle
-==========
-
-A Symfony project created on November 19, 2015, 1:45 am.
--- TestBundle/src/TipBundle/TestBundle/TipBundleTestBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace TipBundle\TestBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TipBundleTestBundle extends Bundle
-{
-}
