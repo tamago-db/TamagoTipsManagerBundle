f7c519fd13914a3c5ba69d9174be6f7815ba6c6d
Evan Owens <evan.owens@tamago-db.com>
2016-03-18 14:12:48 -0400

    Fixes #10 crash on inital state / incorrect use of random_int

--- Controller/TipsManagerController.php
+++ Controller/TipsManagerController.php
@@ -3,25 +3,35 @@
 namespace Tamago\TipsManagerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class TipsManagerController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $repositoryTransUnit = $this->getDoctrine()->getManager()->getRepository('LexikTranslationBundle:TransUnit');
-        $count = $repositoryTransUnit->count();
-        $random = random_int(1, $count);
-        $transUnit = $repositoryTransUnit->find($random);
 
+        $transUnits = $repositoryTransUnit->findByDomain('tips-general');
+
+        // 404 if not tips
+        if (!$total = count($transUnits)) {
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+
+        // Get a random tip
+        $random = random_int(0, $total-1);
+        $transUnit = $transUnits[$random];
+
+        // Retrieve meta data
         $repositoryTamago = $this->getDoctrine()->getManager()->getRepository('TamagoTipsManagerBundle:TamagoTransUnitMeta');
-        $request = $this->get('request');
         $metaEntity = $repositoryTamago->singleton($transUnit, $request->getLocale());
 
+        // Increment view count
         $metaEntity->setViewCount($metaEntity->getViewCount() + 1);
         $this->getDoctrine()->getManager()->flush();
 
-        return $this->render('TamagoTipsManagerBundle:Default:index.html.twig', ["tip" => $transUnit]);
+        return $this->render('TamagoTipsManagerBundle:Default:index.html.twig', ['tip' => $transUnit]);
     }
 
     public function feedbackAction($id, $feedback)
